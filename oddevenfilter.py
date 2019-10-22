0d13ba2fc7403b18635ef0375f4752df0691639c
ATUL KUMAR PATEL <45199617+gndit@users.noreply.github.com>
2019-10-23 00:33:23 +0530

    Add files via upload

---
+++ oddevenfilter.py
@@ -0,0 +1,16 @@
+def odd_even(l):
+    odd=[]
+    even =[]
+    for i in range(len(l)):
+        if i%2 == 0 :
+           k = return i
+           even.append(k)
+        x=return i
+        odd.append(x)
+        num = (even , odd)
+        print(num)
+s1 = [1,2,3,4,5,6,7,8,9,11]
+odd_even(s1)
+
+        
+
