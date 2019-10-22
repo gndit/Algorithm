4c8d5c7157ff99c3818942b80085cc293d88e261
ATUL KUMAR PATEL <45199617+gndit@users.noreply.github.com>
2019-10-23 00:29:44 +0530

    Add files via upload

--- linkedlist.py
+++ linkedlist.py
@@ -1,42 +1,70 @@
-# A simple Python program for traversal of a linked list
-
-# Node class
 class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
 
-	# Function to initialise the node object
-	def __init__(self, data):
-		self.data = data # Assign data
-		self.next = None # Initialize next as null
+class Linkedlist:
+    def __init__(self):
+        self.head=None
+
+    def print_node(self):
+        cur_node=self.head
+        while cur_node:
+            print(cur_node.data)
+            cur_node=cur_node.next
+    def insert(self,data):
+        cur_node = Node(data)
+
+        if self.head is None:
+            self.head=cur_node
+            return
+        last_node=self.head
+        while last_node.next:
+            last_node=last_node.next
+        last_node.next=cur_node
+
+    def prepend(self,data):
+        new_node=Node(data)
+        new_node.next=self.head
+        self.head=new_node
+    def insert_after_node(self,pre_node,data):
+        if not pre_node:
+            print("previous node not in list")
+            return
+        new_node = Node(data)
+        new_node.next=pre_node.next
+        pre_node.next=new_node
+    def delt_pre(self):
+        temp = self.head
+        self.head=temp.next
+        temp.next=None
+        return
+    def post_delt(self):
+        temp=self.head
+        while temp.next.next:
+            temp=temp.next
+        temp.next=None
+        return
+    def delt_after_node(self,key):
+        temp=self.head
+        while temp.data!=key:
+            temp=temp.next
+        temp.next=temp.next.next
+        temp.next.next=None
+        return
 
 
-# Linked List class contains a Node object
-class LinkedList:
-
-	# Function to initialize head
-	def __init__(self):
-		self.head = None
-
-	# This function prints contents of linked list
-	# starting from head
-	def printList(self):
-		temp = self.head
-		while (temp):
-
-			print(temp.data)
-			temp = temp.next
+llist=Linkedlist()
 
 
-# Code execution starts here
-if __name__=='__main__':
+llist.insert("A")
+llist.insert("B")
+llist.insert("C")
 
-	# Start with the empty list
-	llist = LinkedList()
-
-	llist.head = Node(1)
-	second = Node(2)
-	third = Node(3)
-
-	llist.head.next = second; # Link first node with second
-	second.next = third; # Link second node with the third node
-
-	llist.printList()
+llist.prepend(3)
+#llist.delt_pre()
+#llist.post_delt()
+#llist.delt_after_node("A")
+llist.insert_after_node(llist.head.next,7)
+llist.print_node()
+print(llist.head.data)
